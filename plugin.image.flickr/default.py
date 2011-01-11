45e2e36c0561c8beee4a0c727f69dc7fa548f946
ruuk <xbmc@2ndmind.net>
2011-01-11 20:55:59 +0000

    Bumped version

--- plugin.image.flickr/default.py
+++ plugin.image.flickr/default.py
@@ -9,8 +9,8 @@ from urllib2 import HTTPError, URLError
 __plugin__ = 'flickr'
 __author__ = 'ruuk'
 __url__ = 'http://code.google.com/p/flickrxbmc/'
-__date__ = '11-10-2010'
-__version__ = '0.9.8'
+__date__ = '01-11-2011'
+__version__ = '0.9.9'
 __settings__ = xbmcaddon.Addon(id='plugin.image.flickr')
 __language__ = __settings__.getLocalizedString
 
